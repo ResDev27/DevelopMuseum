39b34e6371663cbcb09aa03acae290a1021c4099
Ginny <virge.vier@gmail.com>
2022-10-26 15:58:55 +0200

    character animation

--- Js/DevelopMuseum.js
+++ Js/DevelopMuseum.js
@@ -1,7 +1,13 @@
 let config = {
+
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    scale: {
+        width: 800,
+        height: 600,
+        parent: "game"
+    },
     physics: {
         default: 'arcade'
     },
@@ -13,86 +19,111 @@ let config = {
     audio: {
         disableWebAudio: true
     }
+
 };
 
 let game = new Phaser.Game(config);
 
-let octopusSprite;
-let backGround;
-let octopusAnimation, crabyAnimation, cuteStarAnimation, shrimpAnimation;
-let octopusAnim, crabyAnim, cuteStarAnim, shrimpAnim;
 
 function preload ()
 {
-    this.load.image('backGround', './assets/images/bg_water.png');
-    this.load.spritesheet('octopusSprite', './assets/images/octopus.png', { 
-        frameWidth: 24,
-        frameHeight: 32
+    this.load.spritesheet('character', 'Assets/Images/character.png', {
+        frameWidth: 65,
+        frameHeight: 98,
     });
-
-    this.load.spritesheet('crabySprite', './assets/images/craby.png', { 
-        frameWidth: 32,
-        frameHeight: 32
-    });
-
-    this.load.spritesheet('cuteStarSprite', './assets/images/cuteStar.png', { 
-        frameWidth: 34,
-        frameHeight: 32
-    });
-
-    this.load.spritesheet('shrimpSprite', './assets/images/shrimp.png', { 
-        frameWidth: 32,
-        frameHeight: 32
-    });
-
-
 }
 
 function create ()
 {
-    backGround = this.add.image(0,0,'backGround');
-    backGround.setOrigin(0,0);
+    //------------- Variable to monitor the movement ----------------------//
 
-    octopusAnimation = this.anims.create({
-        key: 'jumpyOctopus',
-        frames: this.anims.generateFrameNumbers('octopusSprite'),
-        frameRate: 10,
+    leftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    upKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    downKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+
+
+    //---- ANIMATION ------ UP -----------------------------------------------//
+
+    this.anims.create({
+        key: "up",
+        frameRate: 2,
+        frames: this.anims.generateFrameNumbers("character", {start: 4, end:7}),
         repeat: -1
-        });
-    octopusAnim = this.add.sprite(400, 300, 'octopusSprite');
-    octopusAnim.play('jumpyOctopus');
+    });
 
-    crabyAnimation = this.anims.create({
-        key: 'jumpyCraby',
-        frames: this.anims.generateFrameNumbers('crabySprite'),
-        frameRate: 10,
+    character = this.add.sprite(100, 450, 'character');
+    character.anims.play("up");
+
+    //----CREATION ANIMATION ------ DOWN ---------------------------------------//
+
+    this.anims.create({
+        key: "down",
+        frameRate: 2,
+        frames: this.anims.generateFrameNumbers("character", {start: 0, end:3}),
         repeat: -1
-        });
-    crabyAnim = this.add.sprite(300, 300, 'crabySprite');
-    crabyAnim.play('jumpyCraby');
+    });
 
-    cuteStarAnimation = this.anims.create({
-        key: 'jumpyCuteStar',
-        frames: this.anims.generateFrameNumbers('cuteStarSprite'),
-        frameRate: 10,
+    character.anims.play("down");
+
+    //-----CREATION ANIMATION ----- RIGHT ----------------------------------------//
+
+    this.anims.create({
+        key: "right",
+        frameRate: 2,
+        frames: this.anims.generateFrameNumbers("character", {start: 12, end:14}),
         repeat: -1
-        });
-    cuteStarAnim = this.add.sprite(600, 300, 'cuteStarSprite');
-    cuteStarAnim.play('jumpyCuteStar');
+    });
 
-    shrimpAnimation = this.anims.create({
-        key: 'jumpyShrimp',
-        frames: this.anims.generateFrameNumbers('shrimpSprite'),
-        frameRate: 10,
+    character.anims.play("right");
+
+    //----CREATION ANIMATION ------ LEFT ----------------------------------------------
+
+    this.anims.create({
+        key: "left",
+        frameRate: 2,
+        frames: this.anims.generateFrameNumbers("character", {start: 8, end:10}),
         repeat: -1
-        });
-    shrimpAnim = this.add.sprite(550, 300, 'shrimpSprite');
-    shrimpAnim.play('jumpyShrimp');
+    });
+
+    character.anims.play("left");
+
+    this.anims.create({
+        key: "idle",
+        frameRate: 2,
+        frames: this.anims.generateFrameNumbers("character", {start: 0, end:0}),
+        repeat: -1
+    });
+
+    character.anims.play("idle");
 
 
-
-
+    //----------------------------------------------
 }
 
 function update ()
-{}
+{
+    // -----------------------Update movement --------------------
+
+    if(Phaser.Input.Keyboard.JustDown(downKey)){
+        character.anims.play("down");
+    }
+
+    if(Phaser.Input.Keyboard.JustDown(upKey)){
+        character.anims.play("up");
+    }
+
+    if(Phaser.Input.Keyboard.JustDown(rightKey)){
+        character.anims.play("right");
+    }
+
+    if(Phaser.Input.Keyboard.JustDown(leftKey)){
+        character.anims.play("left");
+    }
+    if(Phaser.Input.Keyboard.JustUp()){
+        character.anims.play("idle");
+    }
+
+
+
+    }
