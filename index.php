9feb5772701a7a5735ce16d2adcfa83432f13890
Chaimae.ELB <elbardiaichaimae@gmail.com>
2022-10-26 22:57:32 +0200

    2css inscription

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="./assets/css/background.css">
+    <link rel="stylesheet" href="DevelopMuseum\assets\css\inscription.css">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DevelopMuseum</title>
 </head>
