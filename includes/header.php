338caebe0648bb9bce3b707f02c09bf6891137a8
boruchowitchJ <boru_jennifer@hotmail.fr>
2022-10-26 22:09:01 +0200

    header

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,11 @@
 
-<header class="nav">
- <img src="./assets/images/logo final.png" alt="logo">
-    <nav>
-      <li> <a href="./index.php?p=inscription">Billetterie</a></li>
+<header >
+
+    <nav class='nav'> 
+      <div><img src="./assets/images/logo final.png" alt="logo"></div>
+      <ul><li> <a href="./index.php?p=inscription">Billetterie</a></li>
       <li><a href="./index.php?p=jeu"> Jeu</a></li>
-      <li><a href="./index.php?p=points">Score</a></li>
+      <li><a href="./index.php?p=points">Score</a></li></ul>
+      
      </nav>
 </header>
