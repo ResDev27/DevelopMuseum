6b8723d9bc6b22aeee9bd2947a1b070ae9a851a4
boruchowitch jennifer <boru_jennifer@hotmail.fr>
2022-10-27 15:00:45 +0200

    finalisation inscritption

--- inscription.php
+++ inscription.php
@@ -16,7 +16,44 @@
 
     <main>
     <div class="login-box">
-    <h2> Check in and get your badge </h1>
+
+<div class="titre">
+  <span class="letter">C</span>
+  <span class="letter">h</span>
+  <span class="letter">e</span>
+  <span class="letter">c</span>
+  <span class="letter">k</span>
+  <span class="letter">-</span>
+ 
+  <em>
+    <span class="letter">i</span>
+    <span class="letter">n</span>
+    <br>  
+  </em>
+  <span class="space"></span>
+  <span class="letter">a</span>
+  <span class="letter">n</span>
+  <span class="letter">d</span>
+  <span class="space"></span>
+  <span class="letter">g</span>
+  <span class="letter">e</span>
+  <span class="letter">t</span>
+  <span class="space"></span>
+
+  <strong>
+    <span class="letter">y</span>
+    <span class="letter">o</span>
+    <span class="letter">u</span>
+    <span class="letter">r</span>
+  </strong>
+  <span class="space"></span>
+  <span class="letter">B</span>
+  <span class="letter">a</span>
+  <span class="letter">d</span>
+  <span class="letter">g</span>
+  <span class="letter">e</span>
+  <span class="space"></span>
+</div>
     <form action="./inscriptionTraitement.php" method="POST">
         <div class="user-box">
         Pseudo:<input type="text" name="pseudo">
@@ -28,10 +65,10 @@
         Date:<input type="date" name="date" value="<?=date("Y-m-d H:i:s");?>">
         </div>
         <a href="#">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
+      <span class="ligne"></span>
+      <span class="ligne"></span>
+      <span class="ligne"></span>
+      <span class="ligne"></span>
       <!-- votre badge -->
      <input id="btn-valid" type="submit" value="check in">
     </a>
