db7778eec8918d4986cf8ff36ace1415fa487b1c
Ginny <virge.vier@gmail.com>
2022-10-27 09:01:17 +0200

    Modif move character

--- Js/DevelopMuseum.js
+++ Js/DevelopMuseum.js
@@ -23,6 +23,8 @@ let config = {
 };
 
 let game = new Phaser.Game(config);
+let Keystate = true;
+var cursors;
 
 
 function preload ()
@@ -35,95 +37,92 @@ function preload ()
 
 function create ()
 {
-    //------------- Variable to monitor the movement ----------------------//
-
-    leftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-    rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-    upKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-    downKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
 
 
     //---- ANIMATION ------ UP -----------------------------------------------//
-
+    
     this.anims.create({
         key: "up",
-        frameRate: 2,
+        frameRate: 4,
         frames: this.anims.generateFrameNumbers("character", {start: 4, end:7}),
         repeat: -1
     });
+    
+    character = this.physics.add.sprite(100, 450, 'character');
 
-    character = this.add.sprite(100, 450, 'character');
     character.anims.play("up");
 
     //----CREATION ANIMATION ------ DOWN ---------------------------------------//
-
+    
     this.anims.create({
         key: "down",
-        frameRate: 2,
+        frameRate: 4,
         frames: this.anims.generateFrameNumbers("character", {start: 0, end:3}),
         repeat: -1
     });
-
+    
     character.anims.play("down");
-
+    
     //-----CREATION ANIMATION ----- RIGHT ----------------------------------------//
 
     this.anims.create({
         key: "right",
-        frameRate: 2,
+        frameRate: 4,
         frames: this.anims.generateFrameNumbers("character", {start: 12, end:14}),
         repeat: -1
     });
-
+    
     character.anims.play("right");
-
+    
     //----CREATION ANIMATION ------ LEFT ----------------------------------------------
-
+    
     this.anims.create({
         key: "left",
-        frameRate: 2,
+        frameRate: 4,
         frames: this.anims.generateFrameNumbers("character", {start: 8, end:10}),
         repeat: -1
     });
-
+    
     character.anims.play("left");
-
+    
     this.anims.create({
         key: "idle",
         frameRate: 2,
         frames: this.anims.generateFrameNumbers("character", {start: 0, end:0}),
         repeat: -1
     });
-
+    
     character.anims.play("idle");
-
-
+    
+    //------------- Variable to monitor the movement ----------------------//
+    
+    cursors = this.input.keyboard.createCursorKeys();
+    
     //----------------------------------------------
 }
 
-function update ()
+function update()
 {
     // -----------------------Update movement --------------------
+    
+    if (cursors.left.isDown)
+    {
+        character.setVelocityX(-160);
+        
+        character.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        character.setVelocityX(160);
+        
+        character.anims.play('right', true);
+    }
+    else
+    {
+        character.setVelocityX(0);
 
-    if(Phaser.Input.Keyboard.JustDown(downKey)){
-        character.anims.play("down");
+        character.anims.play('turn');
     }
 
-    if(Phaser.Input.Keyboard.JustDown(upKey)){
-        character.anims.play("up");
-    }
-
-    if(Phaser.Input.Keyboard.JustDown(rightKey)){
-        character.anims.play("right");
-    }
-
-    if(Phaser.Input.Keyboard.JustDown(leftKey)){
-        character.anims.play("left");
-    }
-    if(Phaser.Input.Keyboard.JustUp()){
-        character.anims.play("idle");
-    }
-
-
-
-    }
+        
+}
